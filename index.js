9a379384114b3af423cd6130a0a4a17fd6c3093b
xiaoguan521 <xiaochen1649@gmail.com>
2023-07-07 11:08:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+    document.getElementById('main-content').innerHTML = '服务器已欠费，后会有期'
   }
